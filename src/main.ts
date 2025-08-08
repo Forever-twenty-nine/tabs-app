81b8853aacbf329b0d398b157edda7f51a744bdc
Sebas Echazú <sebastianechazu@outlook.com>
2025-08-08 11:19:11 -0300

    refactor: reorganizar importaciones y agregar configuración de Firebase

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { importProvidersFrom } from '@angular/core';
 import { IonicStorageModule } from '@ionic/storage-angular';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
-
+import { environment } from './environments/environment';
 import { applyInitialTheme } from 'theme';
+//firebase
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
-// Apply the initial theme based on user preference or stored value
 applyInitialTheme();
 
 bootstrapApplication(AppComponent, {
@@ -19,5 +20,13 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     importProvidersFrom(IonicStorageModule.forRoot()),
+    //firebase
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => {
+      const auth = getAuth();
+      auth.useDeviceLanguage();
+      return auth;
+    }),
   ],
 });
